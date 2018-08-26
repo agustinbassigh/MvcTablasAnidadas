22318dfbe8d7d49bb098fb42be5d59fed0192005
agustinbassigh <agustin.bassi@hotmail.com>
2018-08-26 10:41:57 -0300

    Add project files.

---
+++ MvcTablasAnidadas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTablasAnidadas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MvcTablasAnidadas/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTablasAnidadas.Models;
+
+namespace MvcTablasAnidadas.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities contexto = null;
+        public ActionResult Index()
+        {
+            contexto = new NorthwindEntities();
+            List<Orders> ordenes = contexto.Orders.Take(10).ToList();
+            return View(ordenes);
+        }
+
+        public ActionResult Detalle(int ordenId)
+        {
+            contexto = new NorthwindEntities();
+            List<Order_Details> detalle = contexto.Order_Details.Where(m => m.OrderID == ordenId).ToList();
+            return PartialView(detalle);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
